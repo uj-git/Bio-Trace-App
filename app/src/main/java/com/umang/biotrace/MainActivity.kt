de0d97a30bf4f64996b1072d8a50997ac4b43db4
Umang Jaiswal <umangjaiswal20@gmail.com>
2026-05-08 22:35:51 +0530

    Initial setup MainActivity.kt

--- app/src/main/java/com/umang/biotrace/MainActivity.kt
+++ app/src/main/java/com/umang/biotrace/MainActivity.kt
@@ -1,47 +1,70 @@
 package com.umang.biotrace
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.umang.biotrace.ui.theme.BioTraceTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.navigation.compose.rememberNavController
+import com.umang.biotrace.presentation.navigation.AppNavGraph
 
 class MainActivity : ComponentActivity() {
+
+    private var permissionsGranted by mutableStateOf(false)
+
+    private val permissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+
+            permissionsGranted = permissions.values.all { it }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
+        requestPermissions()
+
         setContent {
-            BioTraceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            BioTraceApp(
+                permissionsGranted = permissionsGranted
+            )
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun requestPermissions() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BioTraceTheme {
-        Greeting("Android")
+        val permissions = mutableListOf(
+            Manifest.permission.CAMERA
+        )
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+        permissionLauncher.launch(permissions.toTypedArray())
     }
+}
+
+
+@Composable
+fun BioTraceApp(
+    permissionsGranted: Boolean
+) {
+
+    val navController = rememberNavController()
+
+    AppNavGraph(
+        navController = navController,
+        permissionsGranted = permissionsGranted
+    )
 }
