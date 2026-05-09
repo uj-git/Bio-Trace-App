26f995b9cb879080c3f6d01fd815cbbf2ece65a3
Umang Jaiswal <umangjaiswal20@gmail.com>
2026-05-09 12:40:47 +0530

    api repository

---
+++ app/src/main/java/com/umang/biotrace/data/remote/ScanApiService.kt
@@ -0,0 +1,20 @@
+package com.umang.biotrace.data.remote
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+
+interface ScanApiService {
+
+    @POST("api/scans")
+    suspend fun uploadScan(@Body request: ScanUploadRequest): Response<ScanResponse>
+
+    @GET("api/scans")
+    suspend fun getAllScans(): Response<List<ScanResponse>>
+
+    @GET("api/scans/{id}")
+    suspend fun getScanById(@Path("id") id: Long): Response<ScanResponse>
+}
