4d680a3ab9b828f8a0f998735403a78d9199943a
Umang Jaiswal <umangjaiswal20@gmail.com>
2026-05-09 12:56:00 +0530

    added network module to modules in BioTraceApplication.kt

--- app/src/main/java/com/umang/biotrace/BioTraceApplication.kt
+++ app/src/main/java/com/umang/biotrace/BioTraceApplication.kt
@@ -2,6 +2,7 @@ package com.umang.biotrace
 
 import android.app.Application
 import com.umang.biotrace.di.appModule
+import com.umang.biotrace.di.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -10,7 +11,7 @@ class BioTraceApplication : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@BioTraceApplication)
-            modules(appModule)
+            modules(appModule, networkModule)
         }
     }
 }
