1fce76d470950add086e19c5fc23b29fe84f8dfe
Umang Jaiswal <umangjaiswal20@gmail.com>
2026-05-09 12:55:16 +0530

    added Scan repo to appModule

--- app/src/main/java/com/umang/biotrace/di/AppModule.kt
+++ app/src/main/java/com/umang/biotrace/di/AppModule.kt
@@ -3,7 +3,8 @@ package com.umang.biotrace.di
 import com.umang.biotrace.data.CameraInfoProvider
 import com.umang.biotrace.data.CameraMetricStore
 import com.umang.biotrace.data.HandDetectionEngine
-import com.umang.biotrace.data.ImageStorageRepository
+import com.umang.biotrace.data.repository.local.ImageStorageRepository
+import com.umang.biotrace.data.repository.remote.ScanRepository
 import com.umang.biotrace.presentation.capture.CaptureViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -14,5 +15,6 @@ val appModule = module {
     single { CameraInfoProvider(androidContext()) }
     single { CameraMetricStore(androidContext()) }
     single { HandDetectionEngine() }
+    single { ScanRepository(get()) }
     viewModel { CaptureViewModel(get(), get(), get(), get()) }
 }
